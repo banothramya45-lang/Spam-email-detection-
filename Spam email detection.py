0ba4ff57b1500a074881b8ec11288a4f7e9b5546
banothramya45-lang <banothramya45@gmail.com>
2026-06-23 00:11:02 +0530

    Update Spam email detection.py

--- Spam email detection.py
+++ Spam email detection.py
@@ -1,27 +1,5 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-
-emails = [
-    "Congratulations! You won a lottery",
-    "Meeting at 10 AM tomorrow",
-    "Claim your free gift now",
-    "Project submission deadline is today"
-]
-
-labels = [1, 0, 1, 0]  # 1 = Spam, 0 = Not Spam
-
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(emails)
-
-model = MultinomialNB()
-model.fit(X, labels)
-
-test_email = ["Free gift available now"]
-test_vector = vectorizer.transform(test_email)
-
-prediction = model.predict(test_vector)
-
-if prediction[0] == 1:
-    print("Spam Email")
-else:
-    print("Not Spam Email")
+ Enter an email message: Free gift available now
+Spam Email
+ Enter an email message: Project meeting tomorrow
+Not Spam Email
+The Spam Email Detection system successfully classified emails as Spam or Not Spam using the Naive Bayes Machine Learning algorithm.
